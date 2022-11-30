4105b6b5903dd252d8aeb24a4be07608a8f63292
Thomas Musselman <tmusselman@gmail.com>
2022-11-30 15:51:10 -0700

    update vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,6 +79,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   # config.filter_sensitive_data('<api_key_1>') { ENV['api_key_1'] }
-  # config.default_cassette_options = { :record => :new_episodes }
+  config.default_cassette_options = { :record => :new_episodes }
   config.configure_rspec_metadata!
 end
