89d2e77eb34f0236cb18fcc7d098cd6ee34c7444
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-12-04 10:42:11 -0700

    happy path for getting one drink from a bar finished

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,7 +7,7 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :drinks, [Types::Drink], null: false do
+    field :drinks, [Types::DrinkType], null: false do
       argument :bar_id, ID, required: true
     end
 
@@ -15,5 +15,14 @@ module Types
       bar = Bar.find(bar_id)
       bar.drinks
     end
+
+    field :drink, Types::DrinkType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def drink(id:)
+      Drink.find(id)
+    end
+
   end
 end
--- spec/graphql/queries/get_all_drinks_for_a_bar_spec.rb
+++ spec/graphql/queries/get_all_drinks_for_a_bar_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Types::Drink, type: :request do
+RSpec.describe Types::DrinkType, type: :request do
   before :each do
     @user = User.create!(name: "Joe Schmoe", email: "joe.schmoe@gmail.com", location: "New York, NY")
     @bar = Bar.create!(name: "Joe's Bar")
@@ -45,46 +45,46 @@ RSpec.describe Types::Drink, type: :request do
         end
         expected = {"data"=>
         {"drinks"=>
-        [{"id"=>"#{@drink_1.id}",
-        "name"=>"#{@drink_1.name}",
-        "imgUrl"=>"#{@drink_1.img_url}",
-        "steps"=>"#{@drink_1.steps}",
-        "createdAt"=>"#{@drink_1.created_at.iso8601}",
-        "updatedAt"=>"#{@drink_1.updated_at.iso8601}",
-        "bar"=>{"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>"#{@bar.drink_count}"},
-        "ingredients"=>
-        [{"name"=>"#{@ingredient_1.name}",
-        "quantity"=>"#{@ingredient_1.quantity}",
-        "createdAt"=>"#{@ingredient_1.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_1.updated_at.iso8601}"},
-        {"name"=>"#{@ingredient_2.name}",
-        "quantity"=>"#{@ingredient_2.quantity}",
-        "createdAt"=>"#{@ingredient_2.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_2.updated_at.iso8601}"},
-        {"name"=>"#{@ingredient_3.name}",
-        "quantity"=>"#{@ingredient_3.quantity}",
-        "createdAt"=>"#{@ingredient_3.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_3.updated_at.iso8601}"}]},
-        {"id"=>"#{@drink_2.id}",
-        "name"=>"#{@drink_2.name}",
-        "imgUrl"=>"#{@drink_2.img_url}",
-        "steps"=>"#{@drink_2.steps}",
-        "createdAt"=>"#{@drink_2.created_at.iso8601}",
-        "updatedAt"=>"#{@drink_2.updated_at.iso8601}",
-        "bar"=>{"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>"#{@bar.drink_count}"},
-        "ingredients"=>
-        [{"name"=>"#{@ingredient_4.name}",
-        "quantity"=>"#{@ingredient_4.quantity}",
-        "createdAt"=>"#{@ingredient_4.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_4.updated_at.iso8601}"},
-        {"name"=>"#{@ingredient_5.name}",
-        "quantity"=>"#{@ingredient_5.quantity}",
-        "createdAt"=>"#{@ingredient_5.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_5.updated_at.iso8601}"},
-        {"name"=>"#{@ingredient_6.name}",
-        "quantity"=>"#{@ingredient_6.quantity}",
-        "createdAt"=>"#{@ingredient_6.created_at.iso8601}",
-        "updatedAt"=>"#{@ingredient_6.updated_at.iso8601}"}]}]}}
+          [{"id"=>"#{@drink_1.id}",
+          "name"=>"#{@drink_1.name}",
+          "imgUrl"=>"#{@drink_1.img_url}",
+          "steps"=>"#{@drink_1.steps}",
+          "createdAt"=>"#{@drink_1.created_at.iso8601}",
+          "updatedAt"=>"#{@drink_1.updated_at.iso8601}",
+          "bar"=>{"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>"#{@bar.drink_count}"},
+          "ingredients"=>
+          [{"name"=>"#{@ingredient_1.name}",
+          "quantity"=>"#{@ingredient_1.quantity}",
+          "createdAt"=>"#{@ingredient_1.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_1.updated_at.iso8601}"},
+          {"name"=>"#{@ingredient_2.name}",
+          "quantity"=>"#{@ingredient_2.quantity}",
+          "createdAt"=>"#{@ingredient_2.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_2.updated_at.iso8601}"},
+          {"name"=>"#{@ingredient_3.name}",
+          "quantity"=>"#{@ingredient_3.quantity}",
+          "createdAt"=>"#{@ingredient_3.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_3.updated_at.iso8601}"}]},
+          {"id"=>"#{@drink_2.id}",
+          "name"=>"#{@drink_2.name}",
+          "imgUrl"=>"#{@drink_2.img_url}",
+          "steps"=>"#{@drink_2.steps}",
+          "createdAt"=>"#{@drink_2.created_at.iso8601}",
+          "updatedAt"=>"#{@drink_2.updated_at.iso8601}",
+          "bar"=>{"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>"#{@bar.drink_count}"},
+          "ingredients"=>
+          [{"name"=>"#{@ingredient_4.name}",
+          "quantity"=>"#{@ingredient_4.quantity}",
+          "createdAt"=>"#{@ingredient_4.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_4.updated_at.iso8601}"},
+          {"name"=>"#{@ingredient_5.name}",
+          "quantity"=>"#{@ingredient_5.quantity}",
+          "createdAt"=>"#{@ingredient_5.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_5.updated_at.iso8601}"},
+          {"name"=>"#{@ingredient_6.name}",
+          "quantity"=>"#{@ingredient_6.quantity}",
+          "createdAt"=>"#{@ingredient_6.created_at.iso8601}",
+          "updatedAt"=>"#{@ingredient_6.updated_at.iso8601}"}]}]}}
 
         post '/graphql', params: {query: query_drinks}
         results = JSON.parse(response.body)
