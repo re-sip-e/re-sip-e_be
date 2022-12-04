4779b6bd50c3f7cddea8517cd9663020cebd4ec9
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-12-04 09:49:20 -0700

    happy path, get all drinks via graphql

---
+++ app/graphql/types/drink.rb
@@ -0,0 +1,12 @@
+module Types
+  class Drink < Types::BaseObject
+    field :id, ID, null: true
+    field :name, String
+    field :img_url, String
+    field :steps, String
+    field :bar, Types::BarTypes
+    field :ingredients, [Types::Ingredient], null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :drinks, [Types::Drink], null: false do
+      argument :bar_id, ID, required: true
+    end
+
+    def drinks(bar_id:)
+      bar = Bar.find(bar_id)
+      bar.drinks
     end
   end
 end
