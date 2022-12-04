08ff2f044f7d1db7d2a78f5d84d3a7d15578e88a
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-12-04 09:54:23 -0700

    changed file name to bar_type rather than bar_types to stick to naming convention for graphql

--- app/graphql/types/bar_type.rb
+++ app/graphql/types/bar_type.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Types
-  class BarTypes < Types::BaseObject
+  class BarType < Types::BaseObject
     field :id, ID, null: false
     field :name, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
