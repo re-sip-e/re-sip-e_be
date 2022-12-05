f93581bb69ec5572d1bf35bd9806aa3dfb3a51f0
Thomas Musselman <tmusselman@gmail.com>
2022-12-05 16:03:18 -0700

    add mutation file

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :drink_update, mutation: Mutations::DrinkUpdate
     field :create_drink, mutation: Mutations::CreateDrink
   end
 end
