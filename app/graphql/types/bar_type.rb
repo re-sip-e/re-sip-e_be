792254cfe0c7230fa30cc128db4f9a40acb9f0c0
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-04 17:48:16 -0700

    Feat: Add ablity to retraive drink info for each bar queried

--- app/graphql/types/bar_type.rb
+++ app/graphql/types/bar_type.rb
@@ -4,6 +4,7 @@ module Types
   class BarType < Types::BaseObject
     field :id, ID, null: false
     field :name, String
+    field :drinks, [Types::DrinkType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :drink_count, Integer
