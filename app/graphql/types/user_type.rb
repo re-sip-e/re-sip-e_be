010e2465060873fed6e2d6bfa813ff93187ddb2c
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-04 20:56:30 -0700

    Add bars to users graphQL query field options

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -9,6 +9,7 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :bar_count, Integer 
+    field :bars, [Types::BarType], null:false
 
     def bar_count
       object.bar_count
