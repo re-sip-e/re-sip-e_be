37c4cdba6c64ae7a228c9db150a5d55fb0be0257
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-04 20:52:55 -0700

    Add tests for bar count method on user

--- spec/graphql/queries/existing_database/get_user_info_spec.rb
+++ spec/graphql/queries/existing_database/get_user_info_spec.rb
@@ -3,43 +3,42 @@ require 'rails_helper'
 RSpec.describe Types::UserType, type: :request do
   before :each do
     @user = create(:user)
-    
+
     @bar_1 = create(:bar)
-    @bar_user_1= create(:bar_user, bar: @bar_1, user: @user)
+    @bar_user_1 = create(:bar_user, bar: @bar_1, user: @user)
     create_list(:drink, 2, bar: @bar_1)
 
     @bar_2 = create(:bar)
-    @bar_user_2= create(:bar_user, bar: @bar_2, user: @user)
+    @bar_user_2 = create(:bar_user, bar: @bar_2, user: @user)
     create_list(:drink, 3, bar: @bar_2)
   end
 
   describe 'happy path' do
     describe 'get info for a user' do
       it 'can query for info about a user using the users id' do
-
         def query_user
           <<~GQL
-          query {
-            user(id: "#{@user.id}") {
-              id
-              name
-              barCount
-              createdAt
+            query {
+              user(id: "#{@user.id}") {
+                id
+                name
+                barCount
+                createdAt
+              }
             }
-          }
           GQL
         end
 
         expected = { "data": {
-                      "user": {
-                        "id": "#{@user.id}",
-                        "name": "#{@user.name}",
-                        "barCount": @user.bar_count,
-                        "createdAt": "#{@user.created_at}"}
-                      }
-                    }
+          "user": {
+            "id": "#{@user.id}",
+            "name": "#{@user.name}",
+            "barCount": @user.bar_count,
+            "createdAt": "#{@user.created_at.iso8601}"
+          }
+        } }
 
-        post '/graphql', params: {query: query_bar}
+        post '/graphql', params: { query: query_user }
         result = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
@@ -47,51 +46,48 @@ RSpec.describe Types::UserType, type: :request do
       end
 
       it 'can query for all the bars associated with a user when getting user info' do
-
         def query_user
           <<~GQL
-          query {
-            user(id: "#{@user.id}") {
-              id
-              name
-              barCount
-              createdAt
-              bars {
-              id
-              name
-              drinkCount
+            query {
+              user(id: "#{@user.id}") {
+                id
+                name
+                barCount
+                createdAt
+                bars {
+                id
+                name
+                drinkCount
+                }
               }
             }
-          }
           GQL
         end
 
         expected = { "data": {
-                      "user": {
-                        "id": "#{@user.id}",
-                        "name": "#{@user.name}",
-                        "barCount": @user.bar_count,
-                        "createdAt": "#{@user.created_at}",
-                        "bars": [
-                          {"id": "#{@bar_1.id}",
-                          "name": "#{@bar_1.name}",
-                          "drinkCount": @bar_1.drink_count},
-                          {"id": "#{@bar_2.id}",
-                          "name": "#{@bar_2.name}",
-                          "drinkCount": @bar_2.drink_count}
-                        ]}
-                      }
-                    }
+          "user": {
+            "id": "#{@user.id}",
+            "name": "#{@user.name}",
+            "barCount": @user.bar_count,
+            "createdAt": "#{@user.created_at.iso8601}",
+            "bars": [
+              { "id": "#{@bar_1.id}",
+                "name": "#{@bar_1.name}",
+                "drinkCount": @bar_1.drink_count },
+              { "id": "#{@bar_2.id}",
+                "name": "#{@bar_2.name}",
+                "drinkCount": @bar_2.drink_count }
+            ]
+          }
+        } }
 
-        post '/graphql', params: {query: query_user}
+        post '/graphql', params: { query: query_user }
         result = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
         expect(result).to eq(expected)
         expect(result[:data][:name]).to_not eq(@bar_1.name)
       end
-      
     end
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of :email }
   it { should have_many :bar_users }
   it { should have_many(:bars).through(:bar_users) }
+
+  describe 'instance methods' do
+    describe '#bar_count' do
+      it 'provides the count of bars associated with a given user' do
+        user_1 = create(:user)
+        user_2 = create(:user)
+        user_3 = create(:user)
+        bar_user_1 = create(:bar_user, user: user_1)
+        bar_user_2 = create_list(:bar_user, 3, user: user_2)
+        bar_user_3 = create_list(:bar_user, 5, user: user_3)
+
+        expect(user_1.bar_count).to eq(1)
+        expect(user_2.bar_count).to eq(3)
+        expect(user_3.bar_count).to eq(5)
+      end
+
+      it 'provides a count of 0 if no bars are associated with a given user' do
+        user_1 = create(:user)
+
+        expect(user_1.bar_count).to eq(0)
+      end
+    end
+  end
 end
