7fda3c75d76a8dca8371c56ac7dc0b5ba90083ee
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-12-05 20:31:50 -0700

    added factory bot to get_one_drink_for_a_bar endpoint testing

--- spec/graphql/queries/existing_database/get_one_drink_for_a_bar_spec.rb
+++ spec/graphql/queries/existing_database/get_one_drink_for_a_bar_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Types::DrinkType, type: :request do
   before :each do
-    @user = User.create!(name: "Joe Schmoe", email: "joe.schmoe@gmail.com", location: "New York, NY")
-    @bar = Bar.create!(name: "Joe's Bar")
-    @bar.users << @user
-    @drink_1 = Drink.create!(name: "Negroni", img_url: "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg", steps: "Stir into glass over ice, garnish and serve.", bar: @bar)
-    @ingredient_1 = Ingredient.create!(name: "Gin", quantity: "1 oz", drink: @drink_1)
-    @ingredient_2 = Ingredient.create!(name: "Campari", quantity: "1 oz", drink: @drink_1)
-    @ingredient_3 = Ingredient.create!(name: "Sweet Vermouth", quantity: "1 oz", drink: @drink_1)
-    @drink_2 = Drink.create!(name: "Aviation", img_url: "https://www.thecocktaildb.com/images/media/drink/trbplb1606855233.jpg", steps: "Add all ingredients into cocktail shaker filled with ice. Shake well and strain into cocktail glass. Garnish with a cherry.", bar: @bar)
-    @ingredient_4 = Ingredient.create!(name: "Gin", quantity: "4.5 cl", drink: @drink_2)
-    @ingredient_5 = Ingredient.create!(name: "Lemon Juice", quantity: "1.5 cl", drink: @drink_2)
-    @ingredient_6 = Ingredient.create!(name: "Maraschino Liqueur", quantity: "1.5 cl", drink: @drink_2)
+    @bar = create(:bar)
+    @drinks = create_list(:drink, 3, bar: @bar)
+    @ingredients1 = create_list(:ingredient, 3, drink: @drinks[0])
+    @ingredients2 = create_list(:ingredient, 3, drink: @drinks[1])
   end
 
   describe 'happy path' do
@@ -21,7 +14,7 @@ RSpec.describe Types::DrinkType, type: :request do
       def query_drink
         <<~GQL
           query {
-            drink(id: "#{@drink_1.id}") {
+            drink(id: "#{@drinks[0].id}") {
               id
               name
               imgUrl
@@ -46,27 +39,27 @@ RSpec.describe Types::DrinkType, type: :request do
 
       expected = {"data" =>
                   {"drink" =>
-                    {"id" => "#{@drink_1.id}",
-                     "name" => "#{@drink_1.name}",
+                    {"id" => "#{@drinks[0].id}",
+                     "name" => "#{@drinks[0].name}",
                      "imgUrl" =>
-                      "#{@drink_1.img_url}",
-                     "steps" => "#{@drink_1.steps}",
-                     "createdAt" => "#{@drink_1.created_at.iso8601}",
-                     "updatedAt" => "#{@drink_1.updated_at.iso8601}",
+                      "#{@drinks[0].img_url}",
+                     "steps" => "#{@drinks[0].steps}",
+                     "createdAt" => "#{@drinks[0].created_at.iso8601}",
+                     "updatedAt" => "#{@drinks[0].updated_at.iso8601}",
                      "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=> @bar.drink_count},
                      "ingredients" =>
-                      [{"name" => "#{@ingredient_1.name}",
-                        "quantity" => "#{@ingredient_1.quantity}",
-                        "createdAt" => "#{@ingredient_1.created_at.iso8601}",
-                        "updatedAt" => "#{@ingredient_1.updated_at.iso8601}"},
-                        {"name" => "#{@ingredient_2.name}",
-                         "quantity" => "#{@ingredient_2.quantity}",
-                         "createdAt" => "#{@ingredient_2.created_at.iso8601}",
-                         "updatedAt" => "#{@ingredient_2.updated_at.iso8601}"},
-                        {"name" => "#{@ingredient_3.name}",
-                         "quantity" => "#{@ingredient_3.quantity}",
-                         "createdAt" => "#{@ingredient_3.created_at.iso8601}",
-                         "updatedAt" => "#{@ingredient_3.updated_at.iso8601}"}]
+                      [{"name" => "#{@ingredients1[0].name}",
+                        "quantity" => "#{@ingredients1[0].quantity}",
+                        "createdAt" => "#{@ingredients1[0].created_at.iso8601}",
+                        "updatedAt" => "#{@ingredients1[0].updated_at.iso8601}"},
+                        {"name" => "#{@ingredients1[1].name}",
+                         "quantity" => "#{@ingredients1[1].quantity}",
+                         "createdAt" => "#{@ingredients1[1].created_at.iso8601}",
+                         "updatedAt" => "#{@ingredients1[1].updated_at.iso8601}"},
+                        {"name" => "#{@ingredients1[2].name}",
+                         "quantity" => "#{@ingredients1[2].quantity}",
+                         "createdAt" => "#{@ingredients1[2].created_at.iso8601}",
+                         "updatedAt" => "#{@ingredients1[2].updated_at.iso8601}"}]
                       }
                     }
                   }
