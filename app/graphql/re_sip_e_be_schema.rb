7a41be0935aa791a97f337a1283d4a57d2456599
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-12-07 15:29:50 -0700

    sad/edge case for single drink request from DB

--- app/graphql/re_sip_e_be_schema.rb
+++ app/graphql/re_sip_e_be_schema.rb
@@ -37,4 +37,9 @@ class ReSipEBeSchema < GraphQL::Schema
     # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
     GlobalID.find(global_id)
   end
+
+  rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
+    raise GraphQL::ExecutionError, "#{field.type.unwrap.graphql_name} not found"
+  end
+
 end
--- spec/graphql/queries/existing_database/get_one_drink_for_a_bar_spec.rb
+++ spec/graphql/queries/existing_database/get_one_drink_for_a_bar_spec.rb
@@ -72,5 +72,118 @@ RSpec.describe Types::DrinkType, type: :request do
     end
   end
 
+  describe 'sad path' do
+    it "gets an error if no drink ID is rec'd"  do
+      def query_bad_id_drink
+        <<~GQL
+          query {
+            drink(id: 300) {
+              id
+              name
+              imgUrl
+              steps
+              createdAt
+              updatedAt
+              bar {
+                id
+                name
+                drinkCount
+              }
+              ingredients {
+                name
+                quantity
+                createdAt
+                updatedAt
+              }
+            }
+          }
+        GQL
+      end
+
+      post '/graphql', params: {query: query_bad_id_drink}
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result[:errors][0][:message]).to eq("Drink not found")
+    end
+  end
+
+  describe 'edge case' do
+    it 'does not receive and error when a field is requested errently twice' do
+      def query_double_fields_drink
+        <<~GQL
+          query {
+            drink(id: "#{@drinks[0].id}") {
+              id
+              id
+              name
+              name
+              imgUrl
+              imgUrl
+              steps
+              steps
+              createdAt
+              createdAt
+              updatedAt
+              updatedAt
+              bar {
+                id
+                name
+                drinkCount
+              }
+              bar {
+                id
+                name
+                drinkCount
+              }
+              ingredients {
+                name
+                quantity
+                createdAt
+                updatedAt
+              }
+              ingredients {
+                name
+                quantity
+                createdAt
+                updatedAt
+              }
+            }
+          }
+        GQL
+      end
+
+      expected = {"data" =>
+                  {"drink" =>
+                    {"id" => "#{@drinks[0].id}",
+                     "name" => "#{@drinks[0].name}",
+                     "imgUrl" =>
+                      "#{@drinks[0].img_url}",
+                     "steps" => "#{@drinks[0].steps}",
+                     "createdAt" => "#{@drinks[0].created_at.iso8601}",
+                     "updatedAt" => "#{@drinks[0].updated_at.iso8601}",
+                     "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=> @bar.drink_count},
+                     "ingredients" =>
+                      [{"name" => "#{@ingredients1[0].name}",
+                        "quantity" => "#{@ingredients1[0].quantity}",
+                        "createdAt" => "#{@ingredients1[0].created_at.iso8601}",
+                        "updatedAt" => "#{@ingredients1[0].updated_at.iso8601}"},
+                        {"name" => "#{@ingredients1[1].name}",
+                         "quantity" => "#{@ingredients1[1].quantity}",
+                         "createdAt" => "#{@ingredients1[1].created_at.iso8601}",
+                         "updatedAt" => "#{@ingredients1[1].updated_at.iso8601}"},
+                        {"name" => "#{@ingredients1[2].name}",
+                         "quantity" => "#{@ingredients1[2].quantity}",
+                         "createdAt" => "#{@ingredients1[2].created_at.iso8601}",
+                         "updatedAt" => "#{@ingredients1[2].updated_at.iso8601}"}]
+                      }
+                    }
+                  }
+
+      post '/graphql', params: {query: query_double_fields_drink}
+      result = JSON.parse(response.body)
+
+      expect(result).to eq(expected)
+    end
+  end
 
 end
