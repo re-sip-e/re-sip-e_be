66740389594b04b58d34ad97ee282f8c09219f93
Thomas Musselman <tmusselman@gmail.com>
2022-12-07 16:18:32 -0700

    test: first iteration of drink update test

--- spec/graphql/mutations/drink_update_spec.rb
+++ spec/graphql/mutations/drink_update_spec.rb
@@ -7,63 +7,89 @@ RSpec.describe Mutations::UpdateDrink, type: :request do
       drink = create(:drink)
       ingredients = create_list(:ingredient, 3, drink: drink)
 
-      update_drink_mutation = <<~GQL
-        mutation {
-          updateDrink(input:{
-            id: "#{drink.id}"
-            name: "Updated Name"
-            imgUrl: "#{drink.img_url}"
-            steps: "Updated Steps"
-            ingredients: [
-              {
-                name: "#{ingredients[0].name}"
-                quantity: "#{ingredients[0].quantity}"
-              }
-              {
-                name: "Campari"
-                quantity: "1 oz"
-              }
-              {
-                name: "Updated Ingredient Name"
-                quantity: "#{ingredients[2].quantity}"
-              }
-            ]
-          }){
-            drink {
+      updated_drink_json = <<~JSON
+        {
+          "name": "Modified #{drink.name}",
+          "steps": "#{drink.steps}",
+          "imgUrl": "#{drink.image_url}",
+          "ingredients": [
+            {
+              "id": "#{ingredients[0].id}"
+              "name": "#{ingredients[0].name}",
+              "quantity": "#{ingredients[0].quantity}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[1].id}"
+              "name": "#{ingredients[1].name}",
+              "quantity": "#{ingredients[1].quantity}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[2].id}"
+              "name": "#{ingredients[2].name}",
+              "quantity": "Modified Quantity"
+            },
+            {
+              "id": null,
+              "name": "New Ingredient",
+              "quantity": "New Ingredient Quantity"
+            }
+          ]
+        }
+      JSON
+
+      gql_vars = <<~JSON
+        {
+          "input":{
+            "id": "#{drink.id}",
+            "drinkInput": #{updated_drink_json}
+          }
+        }
+      JSON
+
+      mutation = <<~GQL
+        mutation($input: DrinkUpdateInput!){
+          drinkUpdate(input: $input){
+            drink{
               id
               name
-              imgUrl
               steps
+              imgUrl
               ingredients{
+                id
                 name
                 quantity
               }
             }
-            errors
           }
         }
       GQL
+
+      
+      post '/graphql', params: {query: update_drink_mutation, variables: gql_vars}
+      result = JSON.parse(response.body, symbolize_names: true)
+      
+      updated_drink = Drink.find(drink.id)
 
       expected = {
         data: {
           updateDrink: {
             drink: {
               id: drink.id.to_s,
-              name: "Updated Name",
+              name: "Modified #{drink.name}",
               imgUrl: drink.img_url,
-              steps: "Updated Steps",
+              steps: drink.steps,
               ingredients: [
                 {
-                  name: ingredients[0].name,
-                  quantity: ingredients[0].quantity
+                  id: ingredients[2].id.to_s,
+                  name: ingredients[2].name,
+                  quantity: "Modified Quantity"
                 },
                 {
-                  name: "Campari",
-                  quantity: "1 oz"
-                },
-                {
-                  name: "Updated Ingredient Name",
-                  quantity: ingredients[2].quantity
+                  id: updated_drink.ingredients.last.id.to_s,
+                  name: "New Ingredient",
+                  quantity: "New Ingredient Quantity"
                 }
               ]
             },
@@ -71,33 +97,30 @@ RSpec.describe Mutations::UpdateDrink, type: :request do
           }
         }
       }
-
-      post '/graphql', params: {query: update_drink_mutation}
-      result = JSON.parse(response.body, symbolize_names: true)
-
+      
       expect(response).to be_successful
       expect(result).to eq(expected)
 
-      updated_drink = Drink.find(drink.id)
+      # updated_drink = Drink.find(drink.id)
 
-      expect(updated_drink.name).to_not eq(drink.name)
-      expect(updated_drink.name).to eq("Updated Name")
+      # expect(updated_drink.name).to_not eq(drink.name)
+      # expect(updated_drink.name).to eq("Updated Name")
 
-      expect(updated_drink.img_url).to eq(drink.img_url)
+      # expect(updated_drink.img_url).to eq(drink.img_url)
 
-      expect(updated_drink.steps).to_not eq(drink.steps)
-      expect(updated_drink.steps).to eq("Updated Steps")
+      # expect(updated_drink.steps).to_not eq(drink.steps)
+      # expect(updated_drink.steps).to eq("Updated Steps")
 
-      expect(updated_drink.ingredients[0].name).to eq(ingredients[0].name)
-      expect(updated_drink.ingredients[0].quantity).to eq(ingredients[0].quantity)
+      # expect(updated_drink.ingredients[0].name).to eq(ingredients[0].name)
+      # expect(updated_drink.ingredients[0].quantity).to eq(ingredients[0].quantity)
 
-      expect(updated_drink.ingredients[1]).to_not eq(ingredients[1])
-      expect(updated_drink.ingredients[1].name).to eq("Campari")
-      expect(updated_drink.ingredients[1].quantity).to eq("1 oz")
+      # expect(updated_drink.ingredients[1]).to_not eq(ingredients[1])
+      # expect(updated_drink.ingredients[1].name).to eq("Campari")
+      # expect(updated_drink.ingredients[1].quantity).to eq("1 oz")
 
-      expect(updated_drink.ingredients[2].name).to_not eq(ingredients[2].name)
-      expect(updated_drink.ingredients[2].name).to eq("Updated Ingredient Name")
-      expect(updated_drink.ingredients[2].quantity).to eq(ingredients[2].quantity)
+      # expect(updated_drink.ingredients[2].name).to_not eq(ingredients[2].name)
+      # expect(updated_drink.ingredients[2].name).to eq("Updated Ingredient Name")
+      # expect(updated_drink.ingredients[2].quantity).to eq(ingredients[2].quantity)
     end
   end
 end
