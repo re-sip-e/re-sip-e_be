757b3c7092626237f4b60000c27744c416518745
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-07 21:42:00 -0700

    Update seed to match new ingredient migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ bar.users << user
 
 drink_1 = Drink.create!(name: "Negroni", img_url: "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg", steps: "Stir into glass over ice, garnish and serve.", bar: bar)
 
-ingredient_1 = Ingredient.create!(name: "Gin", quantity: "1 oz", drink: drink_1)
-ingredient_2 = Ingredient.create!(name: "Campari", quantity: "1 oz", drink: drink_1)
-ingredient_3 = Ingredient.create!(name: "Sweet Vermouth", quantity: "1 oz", drink: drink_1)
+ingredient_1 = Ingredient.create!(description: "1 oz Gin", drink: drink_1)
+ingredient_2 = Ingredient.create!(description: "1 oz Campari", drink: drink_1)
+ingredient_3 = Ingredient.create!(description: "1 oz Sweet Vermouth", drink: drink_1)
 
 drink_2 = Drink.create!(name: "Aviation", img_url: "https://www.thecocktaildb.com/images/media/drink/trbplb1606855233.jpg", steps: "Add all ingredients into cocktail shaker filled with ice. Shake well and strain into cocktail glass. Garnish with a cherry.", bar: bar)
 
-ingredient_4 = Ingredient.create!(name: "Gin", quantity: "4.5 cl", drink: drink_2)
-ingredient_5 = Ingredient.create!(name: "Lemon Juice", quantity: "1.5 cl", drink: drink_2)
-ingredient_6 = Ingredient.create!(name: "Maraschino Liqueur", quantity: "1.5 cl", drink: drink_2)
+ingredient_4 = Ingredient.create!(description: "4.5 cl Gin", drink: drink_2)
+ingredient_5 = Ingredient.create!(description: "1.5 cl Lemon Juice", drink: drink_2)
+ingredient_6 = Ingredient.create!(description: "1.5 cl Maraschino Liqueur", drink: drink_2)
 
