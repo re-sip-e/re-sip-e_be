b805c130734407e08b94ce595a14d900efd4e459
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-07 17:39:35 -0700

    Test: Add test for Invalid Drink ID received for API Drinks

--- spec/graphql/queries/api_related/find_drink_with_id_api_spec.rb
+++ spec/graphql/queries/api_related/find_drink_with_id_api_spec.rb
@@ -1,59 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Types::DrinkType, type: :request do
-
-  describe 'happy path' do
-    it 'can find a drink by id from api', :vcr do
-
-      def query_drink
-        <<~GQL
-        query {
-          apiDrink(id: 11003){
-            id
-            name
-            steps
-            imgUrl
-            ingredients {
+  describe 'Find a Drink from Cocktail API' do
+    describe 'happy path' do
+      it 'can find a drink by id from api', :vcr do
+        query_drink = <<~GQL
+          query {
+            apiDrink(id: 11003){
+              id
               name
-              quantity
+              steps
+              imgUrl
+              ingredients {
+                name
+                quantity
+              }
+            }
+          }
+        GQL
+
+        expected = {
+          'data' => {
+            'apiDrink' => {
+              'id' => '11003',
+              'name' => 'Negroni',
+              'steps' => 'Stir into glass over ice, garnish and serve.',
+              'imgUrl' => 'https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg',
+              'ingredients' => [
+                {
+                  'name' => 'Gin',
+                  'quantity' => '1 oz '
+                },
+                {
+                  'name' => 'Campari',
+                  'quantity' => '1 oz '
+                },
+                {
+                  'name' => 'Sweet Vermouth',
+                  'quantity' => '1 oz '
+                }
+              ]
             }
           }
         }
-        GQL
+
+        post '/graphql', params: { query: query_drink }
+        result = JSON.parse(response.body)
+        expect(response).to be_successful
+        expect(result).to eq(expected)
       end
+    end
 
-      expected = {
-        "data"=> {
-          "apiDrink"=> {
-            "id"=> "11003",
-            "name"=> "Negroni",
-            "steps"=> "Stir into glass over ice, garnish and serve.",
-            "imgUrl"=> "https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg",
-            "ingredients"=> [
-              {
-                "name"=> "Gin",
-                "quantity"=> "1 oz ",
-              },
-              {
-                "name"=> "Campari",
-                "quantity"=> "1 oz "
-              },
-              {
-                "name"=> "Sweet Vermouth",
-                "quantity"=> "1 oz "
+    describe 'Sad Paths' do
+      it 'If an Invalid Drink ID for teh API is received an Error is returned', :vcr do
+        query_invalid_drink = <<~GQL
+          query {
+            apiDrink(id: 1){
+              id
+              name
+              steps
+              imgUrl
+              ingredients {
+                name
+                quantity
               }
-            ]
+            }
           }
-        }
-      }
-
-      post '/graphql', params: {query: query_drink}
-      result = JSON.parse(response.body)
-      expect(response).to be_successful
-      expect(result).to eq(expected)
-
+        GQL
+        
+        post '/graphql', params: { query: query_invalid_drink }
+        result = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(result[:errors][0][:message]).to eq("Drink not found")
+      end
     end
   end
-
-
 end
