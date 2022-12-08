045b65584c2d4cfdb7d40869c3b14b20b32ecd6a
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-07 18:21:43 -0700

    Test: Add test for if fields are requested multiple times

--- spec/graphql/queries/api_related/find_drink_with_id_api_spec.rb
+++ spec/graphql/queries/api_related/find_drink_with_id_api_spec.rb
@@ -74,5 +74,60 @@ RSpec.describe Types::DrinkType, type: :request do
         expect(result[:errors][0][:message]).to eq("Drink not found")
       end
     end
+
+    describe 'Edge Cases' do
+      it 'Fields are requested multiple times in query a a valid response is returned', :vcr do
+        query_duplicate_drink = <<~GQL
+          query {
+            apiDrink(id: 11003){
+              id
+              id
+              name
+              name
+              steps
+              steps
+              imgUrl
+              imgUrl
+              ingredients {
+                name
+                name
+                quantity
+                quantity
+              }
+            }
+          }
+        GQL
+        
+        expected = {
+          'data' => {
+            'apiDrink' => {
+              'id' => '11003',
+              'name' => 'Negroni',
+              'steps' => 'Stir into glass over ice, garnish and serve.',
+              'imgUrl' => 'https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg',
+              'ingredients' => [
+                {
+                  'name' => 'Gin',
+                  'quantity' => '1 oz '
+                },
+                {
+                  'name' => 'Campari',
+                  'quantity' => '1 oz '
+                },
+                {
+                  'name' => 'Sweet Vermouth',
+                  'quantity' => '1 oz '
+                }
+              ]
+            }
+          }
+        }
+
+        post '/graphql', params: { query: query_duplicate_drink }
+        result = JSON.parse(response.body)
+        expect(response).to be_successful
+        expect(result).to eq(expected)
+      end
+    end
   end
 end
