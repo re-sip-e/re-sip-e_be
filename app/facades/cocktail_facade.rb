bd6babad179e090084028df721b2bc0c86f7a046
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-07 18:58:31 -0700

    Refactor: Updat all Facade methods to use ingrediant find helper method

--- app/facades/cocktail_facade.rb
+++ app/facades/cocktail_facade.rb
@@ -2,14 +2,7 @@ class CocktailFacade
   def self.by_name(query)
     cocktails = CocktailService.get_search_results(query)[:drinks]
     cocktails.map do |cocktail|
-      ingredients = []
-      15.times do |i|
-        if cocktail["strIngredient#{i+1}".to_sym]
-          name = cocktail["strIngredient#{i+1}".to_sym]
-          measurement = cocktail["strMeasure#{i+1}".to_sym]
-          ingredients << { name: name, measurement: measurement }
-        end
-      end
+      ingredients = find_ingredients(cocktail)
       ingredients.map! do |ingredient|
         Ingredient.new(name: ingredient[:name], quantity: ingredient[:measurement])
       end
@@ -20,18 +13,12 @@ class CocktailFacade
   def self.three_random
       cocktails = CocktailService.get_three_random
       cocktails.map do |cocktail|
-        ingredients = []
-        15.times do |i|
-          if cocktail[:drinks][0]["strIngredient#{i+1}".to_sym]
-            name = cocktail[:drinks][0]["strIngredient#{i+1}".to_sym]
-            measurement = cocktail[:drinks][0]["strMeasure#{i+1}".to_sym]
-            ingredients << { name: name, measurement: measurement }
-          end
-        end
+        cocktail = cocktail[:drinks][0]
+        ingredients = find_ingredients(cocktail)
         ingredients.map! do |ingredient|
           Ingredient.new(name: ingredient[:name], quantity: ingredient[:measurement])
         end
-        Drink.new(id: cocktail[:drinks][0][:idDrink], name: cocktail[:drinks][0][:strDrink], img_url: cocktail[:drinks][0][:strDrinkThumb], steps: cocktail[:drinks][0][:strInstructions], ingredients: ingredients)
+        Drink.new(id: cocktail[:idDrink], name: cocktail[:strDrink], img_url: cocktail[:strDrinkThumb], steps: cocktail[:strInstructions], ingredients: ingredients)
       end
     end
 
