109dce116a9c523ee6f376f542bd538736e36ca4
Aleisha Mork <aleisha.mork@gmail.com>
2022-12-10 23:01:49 -0700

    Test: Add tests for delete drink sad path/edge cases

--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Drink, type: :model do
   it { should validate_presence_of :name }
   it { should belong_to :bar }
-  it { should have_many :ingredients }
+  it { should have_many(:ingredients).dependent(:destroy) }
 end
