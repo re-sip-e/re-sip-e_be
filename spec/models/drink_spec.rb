dcaeff39f202d05546d9bfc43e58a34b88b37ce5
Thomas Musselman <tmusselman@gmail.com>
2022-12-12 13:00:28 -0700

    make test for model validation more thorough

--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe Drink, type: :model do
   it 'validates presense of at least one ingredient' do
     drink = Drink.create(name: "test", steps: "make it", img_url: "img url")
     expect(drink.errors.full_messages).to include("Ingredients must have at least one ingredient")
+    expect(Drink.count).to eq(0)
+
+    drink = Drink.create(name: "test", steps: "make it", img_url: "img url", ingredients_attributes: [])
+    expect(drink.errors.full_messages).to include("Ingredients must have at least one ingredient")
+    expect(Drink.count).to eq(0)
   end
 end
