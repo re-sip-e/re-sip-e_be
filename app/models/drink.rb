02b5abad83cc05802bba14f2ba6810a415bb4610
Thomas Musselman <tmusselman@gmail.com>
2022-12-12 12:58:22 -0700

    add validation to ensure drink has one ingredient

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -5,4 +5,14 @@ class Drink < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   
   accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+  validate :has_ingredients
+
+  private
+
+  def has_ingredients
+    if ingredients.length == 0
+      errors.add(:ingredients, "must have at least one ingredient")
+    end
+  end
 end
