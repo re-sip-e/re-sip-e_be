4327187196f6690cb7463ec955bb1702d5988615
Thomas Musselman <tmusselman@gmail.com>
2022-12-12 16:58:44 -0700

    test: add tests for update drink sad paths

--- spec/graphql/mutations/drink_update_spec.rb
+++ spec/graphql/mutations/drink_update_spec.rb
@@ -225,4 +225,163 @@ RSpec.describe Mutations::DrinkUpdate, type: :request do
       expect(updated_drink.ingredients[1].description).to eq("New Description")
     end
   end
+
+  describe ':( sad path' do
+    let!(:mutation) {
+      <<~GQL
+        mutation($input: DrinkUpdateInput!){
+          drinkUpdate(input: $input){
+            drink{
+              id
+              name
+              steps
+              imgUrl
+              ingredients{
+                id
+                description
+              }
+            }
+          }
+        }
+      GQL
+    }
+
+    let!(:drink){create(:drink)}
+    let!(:ingredients){drink.ingredients}
+
+    it 'returns an error if the drink id is invalid' do
+      updated_drink_json = <<~JSON
+        {
+          "name": "Modified #{drink.name}",
+          "steps": "#{drink.steps}",
+          "imgUrl": "#{drink.img_url}",
+          "ingredients": [
+            {
+              "id": "#{ingredients[0].id}",
+              "description": "#{ingredients[0].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[1].id}",
+              "description": "#{ingredients[1].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[2].id}",
+              "description": "#{ingredients[2].description}"
+            },
+            {
+              "id": null,
+              "description": "New Description"
+            }
+          ]
+        }
+      JSON
+
+      gql_vars = <<~JSON
+        {
+          "input":{
+            "id": "#{drink.id + 1}",
+            "drinkInput": #{updated_drink_json}
+          }
+        }
+      JSON
+
+      post '/graphql', params: {query: mutation, variables: gql_vars}
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expected = {:data=>{:drinkUpdate=>nil}, :errors=>[{:message=>"Couldn't find Drink with 'id'=#{drink.id + 1}", :locations=>[{:line=>2, :column=>3}], :path=>["drinkUpdate"]}]}
+
+      expect(result).to eq(expected)
+    end
+
+    it 'returns an error if incorrect ingredient id is used' do
+      updated_drink_json = <<~JSON
+        {
+          "name": "Modified #{drink.name}",
+          "steps": "#{drink.steps}",
+          "imgUrl": "#{drink.img_url}",
+          "ingredients": [
+            {
+              "id": "#{ingredients[0].id}",
+              "description": "#{ingredients[0].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[1].id}",
+              "description": "#{ingredients[1].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[2].id + 15}",
+              "description": "#{ingredients[2].description}"
+            },
+            {
+              "id": null,
+              "description": "New Description"
+            }
+          ]
+        }
+      JSON
+
+      gql_vars = <<~JSON
+        {
+          "input":{
+            "id": "#{drink.id}",
+            "drinkInput": #{updated_drink_json}
+          }
+        }
+      JSON
+
+      post '/graphql', params: {query: mutation, variables: gql_vars}
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expected = {:data=>{:drinkUpdate=>nil}, :errors=>[{:message=>"Couldn't find Ingredient with ID=#{ingredients[2].id + 15} for Drink with ID=#{drink.id}", :locations=>[{:line=>2, :column=>3}], :path=>["drinkUpdate"]}]}
+
+      expect(result).to eq(expected)
+    end
+
+    it 'returns an error if all the ingredients are destroyed' do
+      updated_drink_json = <<~JSON
+        {
+          "name": "Modified #{drink.name}",
+          "steps": "#{drink.steps}",
+          "imgUrl": "#{drink.img_url}",
+          "ingredients": [
+            {
+              "id": "#{ingredients[0].id}",
+              "description": "#{ingredients[0].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[1].id}",
+              "description": "#{ingredients[1].description}",
+              "_destroy": true
+            },
+            {
+              "id": "#{ingredients[2].id}",
+              "description": "#{ingredients[2].description}",
+              "_destroy": true
+            }
+          ]
+        }
+      JSON
+
+      gql_vars = <<~JSON
+        {
+          "input":{
+            "id": "#{drink.id}",
+            "drinkInput": #{updated_drink_json}
+          }
+        }
+      JSON
+
+      post '/graphql', params: {query: mutation, variables: gql_vars}
+      result = JSON.parse(response.body, symbolize_names: true)
+      
+      expected = {:data=>{:drinkUpdate=>nil}, :errors=>[{:message=>"Error updating drink", :locations=>[{:line=>2, :column=>3}], :path=>["drinkUpdate"], :extensions=>{:ingredients=>["can't be blank"]}}]}
+
+      expect(result).to eq(expected)
+    end
+  end
 end
