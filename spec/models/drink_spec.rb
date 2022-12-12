771e79fd78ea2c4cbe5addebb1c0ccfc67d247ed
Thomas Musselman <tmusselman@gmail.com>
2022-12-12 12:57:56 -0700

    test model validation for drink containing ingredient

--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Drink, type: :model do
   it { should validate_presence_of :name }
   it { should belong_to :bar }
   it { should have_many(:ingredients).dependent(:destroy) }
+
+  it 'validates presense of at least one ingredient' do
+    drink = Drink.create(name: "test", steps: "make it", img_url: "img url")
+    expect(drink.errors.full_messages).to include("Ingredients must have at least one ingredient")
+  end
 end
