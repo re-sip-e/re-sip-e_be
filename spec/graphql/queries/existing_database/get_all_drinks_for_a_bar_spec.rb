0ced77a70a3496b4d505dd37055b80003d633b37
Aleisha Mork <aleisha.mork@gmail.com>
2023-01-29 18:20:36 -0700

    Added test for drinks returned in a sorted order and corrected a issue with destroy all in the before each

--- spec/graphql/queries/existing_database/get_all_drinks_for_a_bar_spec.rb
+++ spec/graphql/queries/existing_database/get_all_drinks_for_a_bar_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Types::DrinkType, type: :request do
   before :each do
     @bar = create(:bar)
-    @bar.drinks.destroy_all
-    @drinks = create_list(:drink, 2, bar: @bar)
-    @ingredients1 = @drinks[0].ingredients
-    @ingredients2 = @drinks[1].ingredients
+    @bar.drinks.first.destroy
+    
+    @drink_1 = create(:drink, name: "Aviation", bar: @bar)
+    @drink_2 = create(:drink, name: "Margarita", bar: @bar)
+    @ingredients1 = @drink_1.ingredients
+    @ingredients2 = @drink_2.ingredients
   end
 
   describe 'happy path' do
@@ -37,12 +39,12 @@ RSpec.describe Types::DrinkType, type: :request do
       end
       expected = {"data" =>
       {"drinks" =>
-        [{"id" => "#{@drinks[0].id}",
-          "name" => "#{@drinks[0].name}",
-          "imgUrl" => "#{@drinks[0].img_url}",
-          "steps" => "#{@drinks[0].steps}",
-          "createdAt" => "#{@drinks[0].created_at.iso8601}",
-          "updatedAt" => "#{@drinks[0].updated_at.iso8601}",
+        [{"id" => "#{@drink_1.id}",
+          "name" => "#{@drink_1.name}",
+          "imgUrl" => "#{@drink_1.img_url}",
+          "steps" => "#{@drink_1.steps}",
+          "createdAt" => "#{@drink_1.created_at.iso8601}",
+          "updatedAt" => "#{@drink_1.updated_at.iso8601}",
           "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=> @bar.drink_count},
           "ingredients" =>
         [{"description" => "#{@ingredients1[0].description}",
@@ -54,12 +56,12 @@ RSpec.describe Types::DrinkType, type: :request do
          {"description" => "#{@ingredients1[2].description}",
           "createdAt" => "#{@ingredients1[2].created_at.iso8601}",
           "updatedAt" => "#{@ingredients1[2].updated_at.iso8601}"}]},
-         {"id" => "#{@drinks[1].id}",
-          "name" => "#{@drinks[1].name}",
-          "imgUrl" => "#{@drinks[1].img_url}",
-          "steps" => "#{@drinks[1].steps}",
-          "createdAt" => "#{@drinks[1].created_at.iso8601}",
-          "updatedAt" => "#{@drinks[1].updated_at.iso8601}",
+         {"id" => "#{@drink_2.id}",
+          "name" => "#{@drink_2.name}",
+          "imgUrl" => "#{@drink_2.img_url}",
+          "steps" => "#{@drink_2.steps}",
+          "createdAt" => "#{@drink_2.created_at.iso8601}",
+          "updatedAt" => "#{@drink_2.updated_at.iso8601}",
           "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>@bar.drink_count},
           "ingredients" =>
          [{"description" => "#{@ingredients2[0].description}",
@@ -71,6 +73,50 @@ RSpec.describe Types::DrinkType, type: :request do
           {"description" => "#{@ingredients2[2].description}",
            "createdAt" => "#{@ingredients2[2].created_at.iso8601}",
            "updatedAt" => "#{@ingredients2[2].updated_at.iso8601}"}]}]}}
+
+      post '/graphql', params: {query: query_drinks}
+      results = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(results).to eq(expected)
+    end
+
+    it 'returns all drinks for a bar in alphabetical order' do
+      @bar.drinks.destroy_all
+      @drink_1 = create(:drink, name: "Whiskey Sour", bar: @bar)
+      @drink_2 = create(:drink, name: "Bloody Mary", bar: @bar)
+      @drink_3 = create(:drink, name: "Bee's Knees", bar: @bar)
+      @drink_4 = create(:drink, name: "Manhattan", bar: @bar)
+
+      def query_drinks
+        <<~GQL
+          query {
+            drinks(barId: "#{@bar.id}") {
+              id
+              name
+              bar {
+                id
+                name
+                drinkCount
+              }
+            }
+          }
+        GQL
+      end
+      expected = {"data" =>
+      {"drinks" =>
+        [{"id" => "#{@drink_3.id}",
+          "name" => "#{@drink_3.name}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=> @bar.drink_count}},
+         {"id" => "#{@drink_2.id}",
+          "name" => "#{@drink_2.name}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>@bar.drink_count}},
+          {"id" => "#{@drink_4.id}",
+          "name" => "#{@drink_4.name}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>@bar.drink_count}},
+          {"id" => "#{@drink_1.id}",
+          "name" => "#{@drink_1.name}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>@bar.drink_count}}]}}
 
       post '/graphql', params: {query: query_drinks}
       results = JSON.parse(response.body)
@@ -116,16 +162,10 @@ RSpec.describe Types::DrinkType, type: :request do
 
   describe 'edge case' do
     it 'will return results even if duplicate fields are queried' do
-      @bar_1 = create(:bar)
-      drink_1 = create(:drink, name: "Aviation", bar: @bar_1)
-      drink_2 = create(:drink, name: "Whiskey Sour", bar: @bar_1)
-      ingredients_1 = drink_1.ingredients
-      ingredients_2 = drink_2.ingredients
-
       def query_dup_fields_for_bar_drinks
         <<~GQL
           query {
-            drinks(barId: "#{@bar_1.id}") {
+            drinks(barId: "#{@bar.id}") {
               id
               id
               name
@@ -165,40 +205,40 @@ RSpec.describe Types::DrinkType, type: :request do
 
       expected = {"data" =>
       {"drinks" =>
-        [{"id" => "#{drink_1.id}",
-          "name" => "#{drink_1.name}",
-          "imgUrl" => "#{drink_1.img_url}",
-          "steps" => "#{drink_1.steps}",
-          "createdAt" => "#{drink_1.created_at.iso8601}",
-          "updatedAt" => "#{drink_1.updated_at.iso8601}",
-          "bar" => {"id"=>"#{@bar_1.id}", "name"=>"#{@bar_1.name}", "drinkCount"=> @bar_1.drink_count},
+        [{"id" => "#{@drink_1.id}",
+          "name" => "#{@drink_1.name}",
+          "imgUrl" => "#{@drink_1.img_url}",
+          "steps" => "#{@drink_1.steps}",
+          "createdAt" => "#{@drink_1.created_at.iso8601}",
+          "updatedAt" => "#{@drink_1.updated_at.iso8601}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=> @bar.drink_count},
           "ingredients" =>
-        [{"description" => "#{ingredients_1[0].description}",
-          "createdAt" => "#{ingredients_1[0].created_at.iso8601}",
-          "updatedAt" => "#{ingredients_1[0].updated_at.iso8601}"},
-         {"description" => "#{ingredients_1[1].description}",
-          "createdAt" => "#{ingredients_1[1].created_at.iso8601}",
-          "updatedAt" => "#{ingredients_1[1].updated_at.iso8601}"},
-         {"description" => "#{ingredients_1[2].description}",
-          "createdAt" => "#{ingredients_1[2].created_at.iso8601}",
-          "updatedAt" => "#{ingredients_1[2].updated_at.iso8601}"}]},
-         {"id" => "#{drink_2.id}",
-          "name" => "#{drink_2.name}",
-          "imgUrl" => "#{drink_2.img_url}",
-          "steps" => "#{drink_2.steps}",
-          "createdAt" => "#{drink_2.created_at.iso8601}",
-          "updatedAt" => "#{drink_2.updated_at.iso8601}",
-          "bar" => {"id"=>"#{@bar_1.id}", "name"=>"#{@bar_1.name}", "drinkCount"=>@bar_1.drink_count},
+        [{"description" => "#{@ingredients1[0].description}",
+          "createdAt" => "#{@ingredients1[0].created_at.iso8601}",
+          "updatedAt" => "#{@ingredients1[0].updated_at.iso8601}"},
+         {"description" => "#{@ingredients1[1].description}",
+          "createdAt" => "#{@ingredients1[1].created_at.iso8601}",
+          "updatedAt" => "#{@ingredients1[1].updated_at.iso8601}"},
+         {"description" => "#{@ingredients1[2].description}",
+          "createdAt" => "#{@ingredients1[2].created_at.iso8601}",
+          "updatedAt" => "#{@ingredients1[2].updated_at.iso8601}"}]},
+         {"id" => "#{@drink_2.id}",
+          "name" => "#{@drink_2.name}",
+          "imgUrl" => "#{@drink_2.img_url}",
+          "steps" => "#{@drink_2.steps}",
+          "createdAt" => "#{@drink_2.created_at.iso8601}",
+          "updatedAt" => "#{@drink_2.updated_at.iso8601}",
+          "bar" => {"id"=>"#{@bar.id}", "name"=>"#{@bar.name}", "drinkCount"=>@bar.drink_count},
           "ingredients" =>
-         [{"description" => "#{ingredients_2[0].description}",
-           "createdAt" => "#{ingredients_2[0].created_at.iso8601}",
-           "updatedAt" => "#{ingredients_2[0].updated_at.iso8601}"},
-          {"description" => "#{ingredients_2[1].description}",
-           "createdAt" => "#{ingredients_2[1].created_at.iso8601}",
-           "updatedAt" => "#{ingredients_2[1].updated_at.iso8601}"},
-          {"description" => "#{ingredients_2[2].description}",
-           "createdAt" => "#{ingredients_2[2].created_at.iso8601}",
-           "updatedAt" => "#{ingredients_2[2].updated_at.iso8601}"}]}]}}
+         [{"description" => "#{@ingredients2[0].description}",
+           "createdAt" => "#{@ingredients2[0].created_at.iso8601}",
+           "updatedAt" => "#{@ingredients2[0].updated_at.iso8601}"},
+          {"description" => "#{@ingredients2[1].description}",
+           "createdAt" => "#{@ingredients2[1].created_at.iso8601}",
+           "updatedAt" => "#{@ingredients2[1].updated_at.iso8601}"},
+          {"description" => "#{@ingredients2[2].description}",
+           "createdAt" => "#{@ingredients2[2].created_at.iso8601}",
+           "updatedAt" => "#{@ingredients2[2].updated_at.iso8601}"}]}]}}
 
       post '/graphql', params: {query: query_dup_fields_for_bar_drinks}
       results = JSON.parse(response.body)
